c0241771199be28a5df5b25182ef8d8af9fd57f6
MrAK Techz <l20cs001@vvcoe.org>
2024-07-11 17:00:08 +0530

    Update config.py

--- functions/config.py
+++ functions/config.py
@@ -22,26 +22,26 @@ from decouple import config
 class Var:
     # Telegram Credentials
 
-    API_ID = config("API_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN", default=None)
-    SESSION = config("SESSION", default=None)
+    API_ID = config("API_ID", default="22615660", cast=int)
+    API_HASH = config("API_HASH", default="906e31e7abb5b7bc2990f8f78e8e4a78")
+    BOT_TOKEN = config("BOT_TOKEN", default="7428457378:AAGEy3MiawGr6N5uXxFyg-J0-IKiuNkSBB0")
+    SESSION = config("SESSION", default="1BVtsOLUBu4HpSq4_DPBHlQSbjjux-LrtG59xAFG93GO6YQ9-yHhaZVPs1MHZYFoPZamwH2vBVv5eQmCX9OMky2x2X61YrQBZM07phag8yU3Qe0yHJ0-qHI2HLfUPGMTgYjQQQI2b87uJYzGEhqM8ZtZGZbf16LnjazDpqGZ_U--r5Ucib_82yQMubyyK9ib00cyRE7ShfThEcekx2Y8-rVnKjr5bOR1ITF6hGJO9TIxOJJXmAkpkFAVC1Y3agrDhcrvIP0jyFNhUmgF9ENs80Wzspfj338A2TEvrdbXIQiHjgmvrADhPeGVTSzlQTWqZ0TLCeTXQSxdl5hvrq_deJbUQpPJDDZo=")
 
     # Database Credentials
 
-    FIREBASE_URL = config("FIREBASE_URL", default=None)
+    FIREBASE_URL = config("FIREBASE_URL", default="https://mrakanime-29308-default-rtdb.firebaseio.com")
     FIREBASE_SERVICE_ACCOUNT_FILE = config(
-        "FIREBASE_SERVICE_ACCOUNT_FILE", default=None
+        "FIREBASE_SERVICE_ACCOUNT_FILE", default="https://gist.githubusercontent.com/MrAKTech/c733258771e265fdc5d1cf51436e626f/raw/6aef7fb1cd92c66e00b9a7c5e6a4eed7a909cd49/service.json"
     )
 
     # Channels Ids
 
-    BACKUP_CHANNEL = config("BACKUP_CHANNEL", default=0, cast=int)
-    MAIN_CHANNEL = config("MAIN_CHANNEL", cast=int)
-    LOG_CHANNEL = config("LOG_CHANNEL", cast=int)
-    CLOUD_CHANNEL = config("CLOUD_CHANNEL", cast=int)
-    FORCESUB_CHANNEL = config("FORCESUB_CHANNEL", default=0, cast=int)
-    OWNER = config("OWNER", default=0, cast=int)
+    BACKUP_CHANNEL = config("BACKUP_CHANNEL", default="-1002203370135", cast=int)
+    MAIN_CHANNEL = config("MAIN_CHANNEL", default="-1002190818424", cast=int)
+    LOG_CHANNEL = config("LOG_CHANNEL", default="-1002155091634", cast=int)
+    CLOUD_CHANNEL = config("CLOUD_CHANNEL", default="-1002186868546", cast=int)
+    FORCESUB_CHANNEL = config("FORCESUB_CHANNEL", default="-1002107568197", cast=int)
+    OWNER = config("OWNER", default="6119769857", cast=int)
 
     # Other Configs
 
@@ -53,4 +53,4 @@ class Var:
     CRF = config("CRF", default="27")
     SEND_SCHEDULE = config("SEND_SCHEDULE", default=False, cast=bool)
     RESTART_EVERDAY = config("RESTART_EVERDAY", default=True, cast=bool)
-    FORCESUB_CHANNEL_LINK = config("FORCESUB_CHANNEL_LINK", default="", cast=str)
+    FORCESUB_CHANNEL_LINK = config("FORCESUB_CHANNEL_LINK", default="https://t.me/MrAK_AnimeZz", cast=str)
